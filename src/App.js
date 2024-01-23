39ca56ba6818db6075ec8cb85a97a713fd79cd22
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-23 22:40:13 +0500

    expense app with context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { AddTransaction } from "./components/AddTransaction";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
